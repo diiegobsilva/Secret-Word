724ed710773ce0e405613c847531be4e0ed34ec4
Diego Batista <88885382+diiegobsilva@users.noreply.github.com>
2023-03-16 17:34:25 -0300

    Finalizando CSS

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,30 @@ import "./Game.css"
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Advinhe a palavra</h1>
+       <h3 className="tip">
+          Dica sobre a palavra: <span>Dica...</span>
+       </h3>
+       <div className="wordContainer">
+          <span className="letter">A</span>
+          <span className="blackSquere"></span>
+       </div>
+       <div className="letterContainer">
+          <p>Tente adivinhar a palavra: </p>
+          <form>
+            <input type="text" name="latter" maxLength="1" required/>
+            <button>Jogar!</button>
+          </form>
+          <div className="wrongLettersContainer">
+            <p>Letras já utilizadas</p>
+            <span>a, </span>
+            <span>b, </span>
+          </div>
+       </div>
     </div>
   )
 }
